8949245d9fffbf90791cac53e672ac5a2636ac49
Henriette Heng <henrietteheng@Henriettes-MacBook-Pro.local>
2019-10-01 15:35:17 +0200

    fixed a bug

--- action_page.php
+++ action_page.php
@@ -4,8 +4,8 @@
 Thank you for signing up to weekly newsletter! We have added the following to our files regarding your interests:
 <br>
 First Name: <?php echo $_POST[name]; ?><br>
-email: <?php echo $_POST[email]; ?><br>
-birthday:<?php echo $_POST[bdate]; ?><br>
+email: <?php echo $_POST[mail]; ?><br>
+birthday:<?php echo $_POST[bday]; ?><br>
 Your interests: :<?php echo $_POST[interests]; ?><br>
 
 <?php if
